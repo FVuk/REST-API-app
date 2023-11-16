4c4733325b839ad7ec053b7cf768f5f602f9d34c
Filip Vukić <filipvukic90@gmail.com>
2023-11-17 00:59:47 +0100

    initial commit

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
-import router from "./api/index.js";
+import contactRouter from "./routes/contact.router";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/api", router);
+app.use("/api", contactRouter);
+app.use("/users");
 
 app.use((_, res, __) => {
   res.status(404).json({
@@ -19,7 +20,6 @@ app.use((_, res, __) => {
     data: "Not found",
   });
 });
-
 app.use((err, _, res, __) => {
   console.log(err.stack);
   res.status(500).json({
@@ -29,21 +29,18 @@ app.use((err, _, res, __) => {
     data: "Internal Server Error",
   });
 });
-
 const PORT = 3000;
-
-const connection = mongoose.connect(process.env.DB_HOST, {
-  dbName: "db-contacts",
+const uriDb = process.env.DB_HOST;
+const connection = mongoose.connect(uriDb, {
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
-
 connection
   .then(() => {
-    console.log("\nDatabase connection successful");
-    app.listen(PORT, () => {
-      console.log(`Server running. App listens on port ${PORT}`);
+    app.listen(PORT, function () {
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
   })
-  .catch((err) => {
-    console.error(`\nServer not running. Error message: [${err}]\n`);
-    process.exit(1);
-  });
+  .catch((err) =>
+    console.log(`Server not running. Error message: ${err.message}`)
+  );
